3c29d8db8292c349be77076ce750831533af1276
Pippo <163149564+pippo-wtf@users.noreply.github.com>
2026-09-25 11:40:18 +0200

    Update restored Folio cask to 0.13.4

--- Casks/folio.rb
+++ Casks/folio.rb
@@ -1,6 +1,6 @@
 cask "folio" do
-  version "0.13.3"
-  sha256 "63413201b650f18d592f59f97dfbad126ca441007689108584186794a6be0abe"
+  version "0.13.4"
+  sha256 "566f099bc2c62672d7d86492d0e4e0a0a382e385ce730696d129eea7dab4c547"
 
   url "https://github.com/pippo-wtf/folio/releases/download/v#{version}/Folio-#{version}.zip"
   name "Folio"
